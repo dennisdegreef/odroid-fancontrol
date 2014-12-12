2c7eea049ed3331ff616b5dddf39217f6a3a1f16
Dennis de Greef <github@link0.net>
2014-12-13 09:49:52 +1030

    Added support for the XU3

--- fancontrol.sh
+++ fancontrol.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-#displa  cursor again at EXIT
+#display cursor again at EXIT
 unhide_cursor() {
     printf '\e[?25h'
     clear
 }
 
-trap unhide_cursor EXIT
+trap unhide_cursor EXIT;
+
+MODEL=$(cat /proc/cpuinfo | grep Hardware | awk '{print $3}');
+
+SYS_FAN_MODE="/sys/devices/platform/odroidu2-fan/fan_mode";
+SYS_PWM_DUTY="/sys/devices/platform/odroidu2-fan/pwm_duty";
+SYS_TEMP="/sys/devices/virtual/thermal/thermal_zone0/temp";
+
+if [ "x${MODEL}" == "xODROID-XU3" ];
+then
+	SYS_FAN_MODE="/sys/devices/odroid_fan.14/fan_mode";
+	SYS_PWM_DUTY="/sys/devices/odroid_fan.14/pwm_duty";
+	SYS_TEMP="/sys/devices/virtual/thermal/thermal_zone0/temp";
+fi
 
 last=0
 last_temp=0
 var=100000
 
-#set auto start_temp to 50
-echo 55 > /sys/devices/platform/odroidu2-fan/start_temp
 mean=80000
 
 #hide cursor
@@ -25,8 +36,8 @@ while true; do
 	printf '\033[;H'
 	sleep 1
 	last_temp=$var
-	var=$(< /sys/devices/virtual/thermal/thermal_zone0/temp)
-	current_speed=$(< /sys/devices/platform/odroidu2-fan/pwm_duty)
+	var=$(< $SYS_TEMP)
+	current_speed=$(< $SYS_PWM_DUTY);
 	echo $var
 
 	#echo "$var,${current_speed:22:3}" >> history
@@ -50,11 +61,11 @@ while true; do
 		fi
 
 		last=1
-		echo manual > /sys/devices/platform/odroidu2-fan/fan_mode
+		echo manual > $SYS_FAN_MODE;
 
-		echo 100 > /sys/devices/platform/odroidu2-fan/pwm_duty
+		echo 100 > $SYS_PWM_DUTY;
 		sleep 1
-		echo 40 > /sys/devices/platform/odroidu2-fan/pwm_duty
+		echo 40 > $SYS_PWM_DUTY;
 
 
 		echo "low speed"
@@ -67,7 +78,7 @@ while true; do
 
 		last=2
 
-		echo auto > /sys/devices/platform/odroidu2-fan/fan_mode
+		echo auto > $SYS_FAN_MODE;
 		echo "auto"
 	fi
 done
